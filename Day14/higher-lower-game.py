78cf14f7bb480545e96ff2cd652fd3df38a52066
SahilDx777 <sahilsaxenadx777@gmail.com>
2023-12-27 23:24:09 +0530

    added day13 & day14

---
+++ Day14/higher-lower-game.py
@@ -0,0 +1,51 @@
+from game_data import data
+import random
+import os
+
+
+def format_data(account):
+    account_name = account["name"]
+    account_desc = account["description"]
+    account_country = account["country"]
+    return f"{account_name}, a {account_desc}, from {account_country}"
+
+
+def check_answer(guess, a_followers, b_followers):
+    """Take the user guess and follower counts and returns if they got it right."""
+    if a_followers > b_followers:
+        return guess == "a"
+    else:
+        return guess == "b"
+
+
+score = 0
+game_should_continue = True
+account_b = random.choice(data)
+
+while game_should_continue:
+
+    account_a = account_b
+    account_b = random.choice(data)
+
+    while account_a == account_b:
+        account_b = random.choice(data)
+
+    print(f"Compare A: {format_data(account_a)}.")
+    print("\n Vs \n")
+    print(f"Compare B: {format_data(account_b)}.")
+
+    guess = input("Who has more followers? Type 'A' or  'B'").lower()
+
+    a_follower_count = account_a["follower_count"]
+    b_follower_count = account_b["follower_count"]
+
+    is_correct = check_answer(guess, a_follower_count, b_follower_count)
+
+    os.system("clear")
+
+    if is_correct:
+        score += 1
+        print(f"You're right! Current score: {score}.")
+    else:
+        game_should_continue = False
+        print(f"You're wrong!. Final score: {score}.")
